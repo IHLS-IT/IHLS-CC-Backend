2894fd0cc80feb3db312bd4f23978809b07bfab9
IHLS-IT <dylan.mcdonald@inhouselegalsolutions.com>
2024-12-10 13:18:19 +0000

    Fix formatting in index.js by adding a newline at the end of the file

--- index.js
+++ index.js
@@ -39,3 +39,5 @@ mongoose
   .connect(process.env.CONNECTION_URL)
   .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
   .catch((error) => console.log(error.message));
+
+  
